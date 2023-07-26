51f0e20cb07b1e587753f8dd692401878ad66ec4
Șulic Octavian <octavius1992@gmail.com>
2023-07-26 17:57:54 +0300

    Add style sheet and icons.

--- ThatWeb/Controllers/CategoryController.cs
+++ ThatWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ThatWeb.Data;
+using ThatWeb.Models;
 
 namespace ThatWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
--- ThatWeb/Migrations/20230726095659_SeeCategoryTable.Designer.cs
+++ ThatWeb/Migrations/20230726095659_SeeCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ThatWeb.Data;
 
@@ -10,9 +11,11 @@ using ThatWeb.Data;
 namespace ThatWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230726095659_SeeCategoryTable")]
+    partial class SeeCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
