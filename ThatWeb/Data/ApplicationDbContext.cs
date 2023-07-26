06ef43ef1f5383972f2bdefc36a128ee974fb562
Șulic Octavian <octavius1992@gmail.com>
2023-07-26 11:58:08 +0300

    Add sql database.

---
+++ ThatWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThatWeb.Models;
+
+namespace ThatWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
