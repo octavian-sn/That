4d25efd086409069a34463d09e498cf1b4f577b1
Șulic Octavian <octavius1992@gmail.com>
2023-07-29 12:38:25 +0300

    Add delete page to razor project.

---
+++ ThatWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ThatWebRazor_Temp.Data;
+using ThatWebRazor_Temp.Models;
+
+namespace ThatWebRazor_Temp.Pages.Categories
+{
+    //BINDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Category categoryFromDb = _db.Categories.Find(id);
+            Category = categoryFromDb;
+        }
+        public IActionResult OnPost(int id)
+        {
+            //Category categoryFromDb = _db.Categories.Find(Category);
+            _db.Categories.Remove(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
