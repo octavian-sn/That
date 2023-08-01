e7c87ebcb6a5bfaef2579c189a63ec4326ab0570
Șulic Octavian <octavius1992@gmail.com>
2023-08-01 17:20:34 +0300

    Implement .NET areas.

--- ThatWeb/Areas/Admin/Controllers/CategoryController.cs
+++ ThatWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -2,11 +2,12 @@
 using That.DataAccess.Repository.IRepository;
 using That.Models;
 
-namespace ThatWeb.Controllers
+namespace ThatWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
-        private readonly IUnitOfWork  _unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
         public CategoryController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -18,7 +19,7 @@ namespace ThatWeb.Controllers
             return View(objCategoryList);
         }
 
-        public IActionResult Create() 
+        public IActionResult Create()
         {
             return View();
         }
@@ -31,12 +32,12 @@ namespace ThatWeb.Controllers
                 ModelState.AddModelError("name", "Name cannot be identical to display order.");
             }
 
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-            _unitOfWork.Category.Add(obj);
-            _unitOfWork.Save();
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "The Category has been created successfully.";
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
             }
             return View();
         }
@@ -45,7 +46,7 @@ namespace ThatWeb.Controllers
         {
             if (id == null || id == 0) return NotFound();
 
-            Category categoryFromDb = _unitOfWork.Category.Get(u=> u.Id == id);
+            Category categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
             //Category categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
             //Category categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
 
@@ -60,7 +61,7 @@ namespace ThatWeb.Controllers
             {
                 _unitOfWork.Category.Update(obj);
                 _unitOfWork.Save();
-                    TempData["success"] = "The Category has been updated successfully.";
+                TempData["success"] = "The Category has been updated successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -73,7 +74,7 @@ namespace ThatWeb.Controllers
             Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
 
             if (categoryFromDb == null) return NotFound();
-             
+
             return View(categoryFromDb);
         }
         [HttpPost, ActionName("Delete")]
@@ -85,7 +86,7 @@ namespace ThatWeb.Controllers
             if (categoryFromDb == null) return NotFound();
             _unitOfWork.Category.Remove(categoryFromDb);
             _unitOfWork.Save();
-                TempData["success"] = "The Category has been deleted successfully.";
+            TempData["success"] = "The Category has been deleted successfully.";
             return RedirectToAction("Index");
         }
     }
