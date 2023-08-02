8106ffa5ff613f9de897ccdbe97d78a9d8cb9c30
Șulic Octavian <octavius1992@gmail.com>
2023-08-02 17:16:27 +0300

    Add Product Controller.

--- That.DataAccess/Migrations/20230801161456_SeedProductTable.Designer.cs
+++ That.DataAccess/Migrations/20230801161456_SeedProductTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using That.DataAccess.Data;
 
@@ -10,9 +11,11 @@ using That.DataAccess.Data;
 namespace That.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230801161456_SeedProductTable")]
+    partial class SeedProductTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ That.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using That.DataAccess.Data;
+using That.DataAccess.Repository.IRepository;
+using That.Models;
+
+namespace That.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}
