f5f34866ff222b86993cd5abb637fa9280822eb7
Șulic Octavian <octavius1992@gmail.com>
2023-08-02 17:16:27 +0300

    Add Product Controller.

---
+++ ThatWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using That.DataAccess.Repository.IRepository;
+using That.Models;
+
+namespace ThatWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (product.Title == product.Author) 
+            {
+                ModelState.AddModelError("title", "Title cannot be identical to Author.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "The Product has been added successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            Product productFromDb = _unitOfWork.Product.Get(item => item.Id == id);
+            if (productFromDb == null) return NotFound();
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "The Product has been updated successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            Product? product = _unitOfWork.Product.Get(item => id == item.Id);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            if (product == null) return NotFound();
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "The Product has been removed successfully.";
+            return RedirectToAction("Index");
+        }
+    }
+}
