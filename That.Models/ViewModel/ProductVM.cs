48d4aa585e3205ea77ecb992caf2147442cec78d
Șulic Octavian <octavius1992@gmail.com>
2023-08-04 19:20:23 +0300

    Update Create view to use the new View model.

--- That.Models/ViewModel/ProductVM.cs
+++ That.Models/ViewModel/ProductVM.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 
 namespace That.Models.ViewModel
 {
