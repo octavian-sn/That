ea529c6a6bdcdee5c68d38d476dffa96fcacf24b
Șulic Octavian <octavius1992@gmail.com>
2023-08-08 17:29:16 +0300

    Extend Identity user via ApplicationUser and update db.

--- That.DataAccess/Migrations/20230808142019_extendIdentityUsers.Designer.cs
+++ That.DataAccess/Migrations/20230808142019_extendIdentityUsers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using That.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using That.DataAccess.Data;
 namespace That.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230808142019_extendIdentityUsers")]
+    partial class extendIdentityUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
